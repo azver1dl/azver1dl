fdf6e8327df54818c3349067b0a58d17a215e53a
azver1dl <evgeniy.dylevskiy2006@gmail.com>
2024-12-23 19:16:06 +0300

    Add files via upload

---
+++ лаба 7/404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Welcome to G2 Esports, one of the leading global esports and entertainment brands. Home to legendary players, creators, and entertainers.">
+  <meta name="keywords" content="G2,киберспорт,major,counter strike 2,контр страйк,niko,monesy,нико,монеси,">
+  <title>G2 Esports CS2</title>
+  <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+  <link rel="icon" href="<?php echo get_template_directory_uri(). './G2.ico' ?>" type="image/x-icon">
+</head>
+<body>
+  <?php get_header(); ?>
+  <section class="404">
+    <h1>Упс... 404</h1>
+    <p>Проверьте набранный URL</p>
+  </section>
+  <?php get_footer(); ?>
+<script src="<?php echo get_template_directory_uri(). './alert.js' ?>"></script>
+</body>
+</html>
